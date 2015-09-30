f1b376454fa444c829eeba1783402616c6c7206e
Gabriel Tagliari <gabriel.tagliari93@gmail.com>
2015-09-29 22:27:04 -0300

    Versão 29

--- src/entity/PedidoProduto.java
+++ src/entity/PedidoProduto.java
@@ -17,6 +17,28 @@ public class PedidoProduto {
     private Produto produto;
     
     private int quantidade;
- 
-    // get and set
+
+	public Pedido getPedido() {
+		return pedido;
+	}
+
+	public void setPedido(Pedido pedido) {
+		this.pedido = pedido;
+	}
+
+	public Produto getProduto() {
+		return produto;
+	}
+
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
 }
