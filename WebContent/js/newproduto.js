dfa2ceb488a6a84a3b462d403d12e77a0aaf03fa
Gabriel Tagliari <gabriel.tagliari93@gmail.com>
2015-10-27 11:22:00 -0200

    OTIMIZADO

---
+++ WebContent/js/newproduto.js
@@ -0,0 +1,24 @@
+function limpa_form(){	
+    	$(':input','#form')
+    	 .not(':button, :submit, :reset, :hidden')
+    	 .val('')
+    	 .removeAttr('checked')
+    	 .removeAttr('selected');
+    }
+$(document).ready(function(){
+	$('#form').submit(function(){
+		swal({   
+			title: "Produto Cadastrado!",   
+			text: "",   
+			type: "",   
+			showCancelButton: false,   
+			confirmButtonColor: "#141E5F",   
+			confirmButtonText: "Sim",   
+			cancelButtonText: "No",   
+			closeOnConfirm: true,   
+			closeOnCancel: false }, 
+			function(isConfirm){   
+				window.location = "newproduto.html";
+			});
+	});
+});
